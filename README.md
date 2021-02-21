95a9f23c47c4455bd3e28df1ce55cd045655f12d
Marta Zaorska <marta.zaorska2@gmail.com>
2021-02-21 12:41:48 +0100

    PWA Application

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# find_bike_app
+# Find Bike App
+
 Search bike networks in many cities.
+
+## Table of contents
+
+- [General info](#general-info)
+- [Demo](#demo)
+- [Libraries](#libraries)
+- [Contact](#contact)
+
+## General info
+
+PWA application. The user searches for bike networks and checks where there are free bikes or empty slots. The API data used on the website comes from the [CityBikes API](http://api.citybik.es/v2/).
+
+## Demo
+
+See online my project: https://martazaorska.github.io/find_bike_app/
+
+## Libraries
+
+- [FontAwesome](https://fontawesome.com/)
+
+## Contact
+
+Created by [Marta Zaorska](https://martazaorska.github.io/portfolio/).
---
+++ dist/serviceWorker.js
@@ -0,0 +1,41 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+  'index.html',
+  '/css/main.min.css',
+  '/main.js',
+  '/image/undraw_Ride_a_bicycle_2yok.svg',
+  '/image/app_icon_256x256.png',
+  '/image/blue_pin_30x40.png',
+  '/image/pin_30x40.png',
+  'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap',
+  'https://use.fontawesome.com/releases/v5.15.2/css/all.css',
+];
+
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: Activated');
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cache) => cache !== cacheName)
+          .map((cache) => caches.delete(cache))
+      );
+    })
+  );
+});
